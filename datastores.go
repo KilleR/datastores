f4d71404373fcea6dff2a4133ff76efc11d0e4b8
killer01 <richard.kille>
2022-05-10 15:25:21 +0100

    Updated to use generics

--- datastores.go
+++ datastores.go
@@ -1,45 +1,48 @@
 package datastores
+
+import "sync"
+
 //###########################
 // Memory storage structures
 //###########################
 // Generic data store
-type DataStore struct {
+type DataStore[K comparable, V any] struct {
 	sync.RWMutex
-	cache map[string]string
+	cache map[K]V
 }
 
-func NewDataStore() *DataStore {
-	return &DataStore{
-		cache: make(map[string]string),
+func NewDataStore[K comparable, V any]() *DataStore[K, V] {
+	return &DataStore[K, V]{
+		cache: make(map[K]V),
 	}
 }
 
-func (ds *DataStore) set(key string, value string) {
+func (ds *DataStore[K, V]) set(key K, value V) {
 	ds.Lock()
 	defer ds.Unlock()
 
 	ds.cache[key] = value
 }
 
-func (ds *DataStore) get(key string) string {
+func (ds *DataStore[K, V]) get(key K) V {
 	ds.RLock()
 	defer ds.RUnlock()
 
 	return ds.cache[key]
 }
 
-func (ds *DataStore) setAll(data map[string]string) {
+func (ds *DataStore[K, V]) setAll(data map[K]V) {
 	ds.Lock()
 	defer ds.Unlock()
 
 	ds.cache = data
 }
 
-func (ds *DataStore) unset(key string) bool {
+func (ds *DataStore[K, V]) unset(key K) bool {
 	ds.Lock()
 	defer ds.Unlock()
 
- 	_, ok := ds.cache[key]
+	_, ok := ds.cache[key]
 	if ok {
 		delete(ds.cache, key)
 		return true
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/killer/datastores
+
+go 1.18
